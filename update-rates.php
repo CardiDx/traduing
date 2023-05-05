93e83d2e3306a813f51ce9c9a73ef74088fd6743
Vlad <EatLittleKids>
2023-05-05 08:05:56 +0300

    json file path update

---
+++ update-rates.php
@@ -0,0 +1,26 @@
+<?php
+$app_id = '7254c13874c5453b8bded7444beb2dc2';
+$oxr_url = "https://openexchangerates.org/api/latest.json?app_id=" . $app_id;
+
+// Open CURL session:
+$ch = curl_init($oxr_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Get the data:
+$json = curl_exec($ch);
+curl_close($ch);
+
+// Decode JSON response:
+$oxr_latest = json_decode($json, true);
+
+
+if(!isset($oxr_latest['error']) && isset($oxr_latest['rates']) && is_array($oxr_latest['rates']) && !empty($oxr_latest['rates'])){
+    $json_string = json_encode($oxr_latest['rates'], JSON_PRETTY_PRINT);
+    $path = 'mm-trading/public_html/rates.json';
+
+    $fp = fopen($path, 'w');
+    fwrite($fp, $json_string);
+    fclose($fp);
+}
+
+?>
